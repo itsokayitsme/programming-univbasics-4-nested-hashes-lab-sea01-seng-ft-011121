497fffed5eb299b191e401b0151b0f3f4cd9f626
Johnny Cao <jcao0928@gmail.com>
2021-01-02 01:04:59 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,7 +81,11 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+programmer_hash[:yukihiro_matsumoto] = {
+    :known_for => "Ruby",
+    :languages => ["Lisp","C"]
+  }
+  programmer_hash
 end
 
 def changing_alan
